1ce29afdb6e842c49325b9a4adca9cce1d0317f5
大宝 <rgweiyaping@hotmail.com>
2019-11-27 11:06:54 +0800

    Create oracle_helper.py

---
+++ 00_python_base/public_api/oracle_helper.py
@@ -0,0 +1,61 @@
+# -*- coding: utf-8 -*-
+
+"""
+author: BooBooWei
+info: Oracle python3
+"""
+
+# https://oracle.github.io/odpi/doc/installation.html#macos
+# https://github.com/oracle/python-cx_Oracle
+
+# Build-in Modules
+import os
+import json
+
+# 3rd-part Modules
+import cx_Oracle
+
+os.environ['NLS_LANG'] = 'SIMPLIFIED CHINESE_CHINA.UTF8'
+
+
+class OracleHelper:
+    def __init__(self, **kwargs):
+        self.url = kwargs['url']
+        self.port = kwargs['port']
+        self.username = kwargs['username']
+        self.password = kwargs['password']
+        self.service_name = kwargs.get('service_name')
+        self.oracle_sid = kwargs.get('oracle_sid')
+
+        if self.service_name:
+            try:
+                dsn = cx_Oracle.makedsn(self.url, self.port, service_name=self.service_name)
+                self.conn = cx_Oracle.connect(self.username, self.password, dsn)
+                self.cur = self.conn.cursor()
+            except Exception as e:
+                print(str(e))
+                self.error = 1
+            else:
+                self.error = 0
+
+        elif self.oracle_sid:
+            try:
+                dsn = cx_Oracle.makedsn(self.url, self.port, self.oracle_sid)
+                self.conn = cx_Oracle.connect(self.username, self.password, dsn)
+                self.cur = self.conn.cursor()
+            except Exception as e:
+                print(str(e))
+                self.error = 1
+            else:
+                self.error = 0
+
+    def col_query(self, sql):
+        self.cur.execute(sql)
+        return self.cur.fetchall()
+
+    def commit(self):
+        self.conn.commit()
+
+    def close(self):
+        self.cur.close()
+        self.conn.close()
